898369798805db9436207322ef3445d6109ed09c
Soozie <sooziekam219@gmail.com>
2026-02-07 17:35:49 +0900

    edit name error

--- src/engine/registry.js
+++ src/engine/registry.js
@@ -1,10 +1,12 @@
 import { TYPE_KEYS } from "./typeKeys";
 
-import problemSelect from "./handlers/question/select";
+import questionSelect from "./handlers/question/select";
+import questionMathinput from "./handlers/question/mathinput";
 import togetherSelect from "./handlers/together/select";
 
 const REGISTRY = {
   [TYPE_KEYS.QUESTION_SELECT]: questionSelect,
+  [TYPE_KEYS.QUESTION_MATHINPUT]: questionMathinput,
   [TYPE_KEYS.TOGETHER_SELECT]: togetherSelect,
 };
 
